ed26f61ae87c947ade2832647a4641b1a153814e
Kai Schmidt <kaikaliischmidt@gmail.com>
2018-08-21 23:16:28 -0700

    added widgets and text aligns nicely

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 extern crate graphics;
 
-use graphics::{math::Matrix2d, rectangle, Graphics};
+use std::{collections::HashMap, hash::Hash, mem, ops, rc::Rc};
+
+use graphics::{
+    character::CharacterCache, math::Matrix2d, rectangle, text, Graphics, ImageSize, Transformed,
+};
 
 fn add(a: [f64; 2], b: [f64; 2]) -> [f64; 2] {
     [a[0] + b[0], a[1] + b[1]]
@@ -8,10 +12,6 @@ fn add(a: [f64; 2], b: [f64; 2]) -> [f64; 2] {
 
 fn sub(a: [f64; 2], b: [f64; 2]) -> [f64; 2] {
     [a[0] - b[0], a[1] - b[1]]
-}
-
-fn mul2(a: [f64; 2], b: [f64; 2]) -> [f64; 2] {
-    [a[0] * b[0], a[1] * b[1]]
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
@@ -48,73 +48,283 @@ pub enum Orientation {
     Vertical,
 }
 
-#[derive(Debug, Clone, PartialEq, PartialOrd)]
-pub struct Region {
+#[derive(Clone)]
+pub enum Widget<'a, I: 'a, S: 'a>
+where
+    I: Copy + Eq + Hash,
+    S: Sized,
+{
+    SubRegion {
+        subs: Vec<Region<'a, I, S>>,
+        subs_map: HashMap<I, usize>,
+    },
+    Label {
+        string: String,
+        max_text_size: u32,
+        color: [f32; 4],
+    },
+    Button {
+        string: Option<(String, [f32; 4])>,
+        max_text_size: u32,
+        callback: Rc<Box<FnMut(&mut Region<'a, I, S>, &mut S)>>,
+    },
+    Slider {
+        color: [f32; 4],
+        callback: Rc<Box<FnMut(&'a mut Region<'a, I, S>, &'a mut S) -> (f64, f64, &'a mut f64)>>,
+    },
+    EntryField {
+        background_string: Option<(String, [f32; 4])>,
+        max_text_size: u32,
+        color: [f32; 4],
+        callback: Rc<Box<FnMut(&'a mut Region<'a, I, S>, &'a mut S) -> &'a mut String>>,
+    },
+}
+
+static DEFAULT_MAX_TEXT_SIZE: u32 = 100;
+
+impl<'a, I: 'a, S: 'a> Widget<'a, I, S>
+where
+    I: Copy + Eq + Hash,
+    S: Sized,
+{
+    pub fn label(string: &str) -> Widget<'a, I, S> {
+        Widget::Label {
+            string: string.to_string(),
+            max_text_size: DEFAULT_MAX_TEXT_SIZE,
+            color: [1.0, 1.0, 1.0, 1.0],
+        }
+    }
+    pub fn button<F>(string: Option<&str>, callback: F) -> Widget<'a, I, S>
+    where
+        F: 'static + FnMut(&mut Region<'a, I, S>, &mut S),
+    {
+        Widget::Button {
+            string: string.map(|s| (s.to_string(), [1.0, 1.0, 1.0, 1.0])),
+            max_text_size: DEFAULT_MAX_TEXT_SIZE,
+            callback: Rc::new(Box::new(callback)),
+        }
+    }
+    pub fn slider<F>(callback: F) -> Widget<'a, I, S>
+    where
+        F: 'static + FnMut(&'a mut Region<'a, I, S>, &'a mut S) -> (f64, f64, &'a mut f64),
+    {
+        Widget::Slider {
+            color: [1.0, 1.0, 1.0, 1.0],
+            callback: Rc::new(Box::new(callback)),
+        }
+    }
+    pub fn entry_field<F>(
+        background_string: Option<(&str, [f32; 4])>,
+        callback: F,
+    ) -> Widget<'a, I, S>
+    where
+        F: 'static + FnMut(&'a mut Region<'a, I, S>, &'a mut S) -> &'a mut String,
+    {
+        Widget::EntryField {
+            background_string: background_string.map(|(s, c)| (s.to_string(), c)),
+            max_text_size: DEFAULT_MAX_TEXT_SIZE,
+            color: [1.0, 1.0, 1.0, 1.0],
+            callback: Rc::new(Box::new(callback)),
+        }
+    }
+    pub fn with_color(mut self, col: [f32; 4]) -> Self {
+        use self::Widget::*;
+        match self {
+            Label { ref mut color, .. }
+            | Slider { ref mut color, .. }
+            | EntryField { ref mut color, .. } => *color = col,
+            Button { ref mut string, .. } => if let Some((_, color)) = string {
+                *color = col;
+            },
+            SubRegion { .. } => (),
+        }
+        self
+    }
+    pub fn with_max_text_size(mut self, max_size: u32) -> Self {
+        use self::Widget::*;
+        match self {
+            Label {
+                ref mut max_text_size,
+                ..
+            }
+            | EntryField {
+                ref mut max_text_size,
+                ..
+            }
+            | Button {
+                ref mut max_text_size,
+                ..
+            } => *max_text_size = max_size,
+            SubRegion { .. } | Slider { .. } => (),
+        }
+        self
+    }
+    pub fn region(self) -> Region<'a, I, S> {
+        Region::full().with_widget(self)
+    }
+}
+
+#[derive(Clone)]
+pub struct Region<'a, I: 'a, S: 'a>
+where
+    I: Copy + Eq + Hash,
+    S: Sized,
+{
     rect: Rect,
-    subs: Vec<Region>,
+    widget: Widget<'a, I, S>,
     orientation: Orientation,
     spacing: f64,
     color: [f32; 4],
 }
 
-impl From<[f64; 2]> for Region {
-    fn from(pos: [f64; 2]) -> Self {
-        Region {
-            rect: Rect {
-                pos,
-                size: sub([1.0, 1.0], pos),
-            },
-            subs: Vec::new(),
-            orientation: Orientation::Automatic,
-            spacing: 10.0,
-            color: [0.0, 0.0, 0.0, 0.0],
+impl<'a, I, S, R> From<R> for Region<'a, I, S>
+where
+    I: Copy + Eq + Hash,
+    S: Sized,
+    R: IntoIterator<Item = Self>,
+{
+    fn from(regions: R) -> Self {
+        Region::full().with_sub_regions(regions)
+    }
+}
+
+impl<'a, I, S> ops::Index<I> for Region<'a, I, S>
+where
+    I: Copy + Eq + Hash,
+    S: Sized,
+{
+    type Output = Region<'a, I, S>;
+    fn index(&self, i: I) -> &Self::Output {
+        if let Widget::SubRegion {
+            ref subs,
+            ref subs_map,
+        } = self.widget
+        {
+            &subs[subs_map[&i]]
+        } else {
+            panic!("Can only index subregion widgets")
         }
     }
 }
 
-impl Region {
-    pub fn full() -> Region {
+impl<'a, I, S> ops::IndexMut<I> for Region<'a, I, S>
+where
+    I: Copy + Eq + Hash,
+    S: Sized,
+{
+    fn index_mut(&mut self, i: I) -> &mut Self::Output {
+        if let Widget::SubRegion {
+            ref mut subs,
+            ref subs_map,
+        } = self.widget
+        {
+            &mut subs[subs_map[&i]]
+        } else {
+            panic!("Can only index subregion widgets")
+        }
+    }
+}
+
+impl<'a, I, S> Region<'a, I, S>
+where
+    I: Copy + Eq + Hash,
+    S: Sized,
+{
+    pub fn full() -> Self {
         Region {
             rect: Rect {
                 pos: [0.0, 0.0],
                 size: [1.0, 1.0],
             },
-            subs: Vec::new(),
+            widget: Widget::SubRegion {
+                subs: Vec::new(),
+                subs_map: HashMap::new(),
+            },
             orientation: Orientation::Automatic,
             spacing: 10.0,
             color: [0.0, 0.0, 0.0, 0.0],
         }
     }
-    pub fn to(mut self, bottom_right: [f64; 2]) -> Region {
+    pub fn from_to(mut self, top_left: [f64; 2], bottom_right: [f64; 2]) -> Self {
+        self.rect.pos = top_left;
         self.rect.size = sub(bottom_right, self.rect.pos);
         self
     }
-    pub fn with_spacing(mut self, spacing: f64) -> Region {
+    pub fn with_spacing(mut self, spacing: f64) -> Self {
         self.spacing = spacing;
         self
     }
-    pub fn with_color(mut self, color: [f32; 4]) -> Region {
+    pub fn with_color(mut self, color: [f32; 4]) -> Self {
         self.color = color;
         self
     }
-    pub fn with_orientation(mut self, orientation: Orientation) -> Region {
+    pub fn with_orientation(mut self, orientation: Orientation) -> Self {
         self.orientation = orientation;
         self
     }
-    pub fn sub<I: IntoIterator<Item = Region>>(mut self, regions: I) -> Region {
-        self.subs = regions.into_iter().collect();
+    pub fn with_sub_regions<R: IntoIterator<Item = Self>>(
+        mut self,
+        regions: R,
+    ) -> Region<'a, I, S> {
+        self.widget = Widget::SubRegion {
+            subs: regions.into_iter().collect(),
+            subs_map: HashMap::new(),
+        };
         self
     }
-    pub fn draw<G: Graphics>(&self, window_size: [f64; 2], transform: Matrix2d, graphics: &mut G) {
-        self._draw([0.0, 0.0], window_size, transform, graphics);
+    pub fn with_widget(mut self, widget: Widget<'a, I, S>) -> Self {
+        self.widget = widget;
+        self
     }
-    fn _draw<G: Graphics>(
+    pub fn insert_region(&mut self, region: Self) {
+        if let Widget::SubRegion { ref mut subs, .. } = self.widget {
+            subs.push(region);
+        } else {
+            panic!("Cannot insert region into sub region widget");
+        }
+    }
+    pub fn insert_indexed(&mut self, index: I, region: Self) -> Option<Self> {
+        if let Widget::SubRegion {
+            ref mut subs,
+            ref mut subs_map,
+        } = self.widget
+        {
+            if let Some(replace_index) = subs_map.get(&index).cloned() {
+                Some(mem::replace(&mut subs[replace_index], region))
+            } else {
+                subs_map.insert(index, subs.len());
+                subs.push(region);
+                None
+            }
+        } else {
+            panic!("Cannot insert region into sub region widget");
+        }
+    }
+    pub fn draw<T, G, C>(
+        &self,
+        window_size: [f64; 2],
+        transform: Matrix2d,
+        graphics: &mut G,
+        glyphs: &mut C,
+    ) where
+        T: ImageSize,
+        G: Graphics<Texture = T>,
+        C: CharacterCache<Texture = T>,
+    {
+        self._draw([0.0, 0.0], window_size, transform, graphics, glyphs);
+    }
+    fn _draw<T, G, C>(
         &self,
         window_pos: [f64; 2],
         window_size: [f64; 2],
         transform: Matrix2d,
         graphics: &mut G,
-    ) {
+        glyphs: &mut C,
+    ) where
+        T: ImageSize,
+        G: Graphics<Texture = T>,
+        C: CharacterCache<Texture = T>,
+    {
         let act = self.rect.actualize(Rect {
             pos: window_pos,
             size: window_size,
@@ -126,42 +336,78 @@ impl Region {
             graphics,
         );
         let ori = match self.orientation {
-            Orientation::Automatic => if window_size[0] > window_size[1] {
+            Orientation::Automatic => if act.size[0] > act.size[1] {
                 Orientation::Horizantal
             } else {
                 Orientation::Vertical
             },
             _ => self.orientation,
         };
-        let len = self.subs.len() as f64;
-        let sub_win_size = if let Orientation::Vertical = ori {
-            [
-                window_size[0] - 2.0 * self.spacing,
-                (window_size[1] - self.spacing) / len - self.spacing,
-            ]
-        } else {
-            [
-                (window_size[0] - self.spacing) / len - self.spacing,
-                window_size[1] - 2.0 * self.spacing,
-            ]
-        };
-        for (i, sub) in self.subs.iter().enumerate() {
-            sub._draw(
-                if let Orientation::Vertical = ori {
+        use self::Widget::*;
+        match self.widget {
+            SubRegion { ref subs, .. } => {
+                let len = subs.len() as f64;
+                let sub_win_size = if let Orientation::Vertical = ori {
                     [
-                        window_pos[0] + self.spacing,
-                        self.spacing + i as f64 * (sub_win_size[1] + self.spacing),
+                        act.size[0] - 2.0 * self.spacing,
+                        (act.size[1] - self.spacing) / len - self.spacing,
                     ]
                 } else {
                     [
-                        self.spacing + i as f64 * (sub_win_size[0] + self.spacing),
-                        window_pos[1] + self.spacing,
+                        (act.size[0] - self.spacing) / len - self.spacing,
+                        act.size[1] - 2.0 * self.spacing,
                     ]
-                },
-                sub_win_size,
-                transform,
-                graphics,
-            );
+                };
+                for (i, sub) in subs.iter().enumerate() {
+                    sub._draw(
+                        if let Orientation::Vertical = ori {
+                            [
+                                act.pos[0] + self.spacing,
+                                act.pos[1]
+                                    + self.spacing
+                                    + i as f64 * (sub_win_size[1] + self.spacing),
+                            ]
+                        } else {
+                            [
+                                act.pos[0]
+                                    + self.spacing
+                                    + i as f64 * (sub_win_size[0] + self.spacing),
+                                act.pos[1] + self.spacing,
+                            ]
+                        },
+                        sub_win_size,
+                        transform,
+                        graphics,
+                        glyphs,
+                    );
+                }
+            }
+            Label {
+                ref string,
+                max_text_size,
+                color,
+            } => {
+                let mut size = (act.size[1] * 0.9).min(max_text_size as f64);
+                let width = glyphs
+                    .width(size as u32, string)
+                    .unwrap_or_else(|_| panic!("Unable to cache text: {:?}", string));
+                let horiz_offset = (act.size[0] - width).max(0.0) / 2.0;
+                if width > act.size[0] {
+                    size = size * act.size[0] / width;
+                }
+                text(
+                    color,
+                    size.floor() as u32,
+                    string,
+                    glyphs,
+                    transform.trans(
+                        act.pos[0] + horiz_offset,
+                        act.pos[1] + (act.size[1] / 2.0).max(size),
+                    ),
+                    graphics,
+                ).unwrap_or_else(|_| panic!("Unable to draw text: {:?}", string));
+            }
+            _ => (),
         }
     }
 }
