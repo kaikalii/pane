48452b5db7874e219e3a7cec7aa61c641be30fb0
Kai Schmidt <kaikaliischmidt@gmail.com>
2018-10-25 15:07:30 -0700

    added to simple example

--- examples/simple.rs
+++ examples/simple.rs
@@ -8,4 +8,10 @@ fn main() {
         .with_margin(10.0)
         .split_in_half();
     println!("{:#?}", pane);
+
+    let pane: Pane = Pane::new()
+        .with_orientation(Orientation::Horizontal)
+        .with_size([100.0, 100.0])
+        .split_weighted_named(vec![("Tree", 0.2), ("Editor", 0.6), ("Git", 0.2)]);
+    println!("{:#?}", pane["Editor"]);
 }
